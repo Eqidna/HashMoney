ea280a4ca8292ed31d20a3efaacb0ebd68fe1560
The Eqidna Project <134110219+Eqidna@users.noreply.github.com>
2024-03-16 17:06:47 -0400

    Update Eligibility.tsx

--- components/Eligibility.tsx
+++ components/Eligibility.tsx
@@ -30,44 +30,25 @@ export default function AirdropEligibilityChecker() {
         Check Airdrop Eligibility
       </Heading>
       <SimpleGrid columns={1} width="275px" mx="auto">
-        <Card
-          p={2}
-          m={2}
-          bgGradient="linear(to-r, blue.900, blue.500)"
-          borderRadius="md"
-        >
+        <Card p={2} m={2} bgGradient="linear(to-r, blue.900, blue.500)" borderRadius="md">
           <Box textAlign="center" mb={1}>
             {/* Render any additional content here */}
           </Box>
-          <SimpleGrid columns={1} spacing={5}>
-            <Stack spacing={5}>
-              <div>
-                <input
-                  type="text"
-                  value={walletAddress}
-                  onChange={(e) => setWalletAddress(e.target.value)}
-                  placeholder="Enter wallet address"
-                />
-                <Button
-                  colorScheme="gray"
-                  onClick={handleCheckEligibility}
-                  size="m"
-                >
-                  Check Eligibility
-                </Button>
-                {isLoading && <div style={{ color: "white", fontWeight: "bold" }}>Loading...</div>}
-{eligibilityResult !== undefined && (
-  <p style={{ color: "white", fontWeight: "bold" }}>
-    Eligible: {eligibilityResult ? "Yes" : "No"}
-  </p>
-)}
-
-
-              </div>
-            </Stack>
-          </SimpleGrid>
+          <Stack spacing={5}>
+            <input
+              type="text"
+              value={walletAddress}
+              onChange={(e) => setWalletAddress(e.target.value)}
+              placeholder="Enter wallet address"
+            />
+            <Button colorScheme="gray" onClick={handleCheckEligibility} size="m">
+              Check Eligibility
+            </Button>
+            {isLoading && <div style={{ color: "white" }}>Loading...</div>}
+          </Stack>
         </Card>
       </SimpleGrid>
     </Card>
   );
 }
+
